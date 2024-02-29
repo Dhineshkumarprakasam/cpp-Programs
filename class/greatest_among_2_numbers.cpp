e292f368c05a9929ef353db46aeac035f7979bb2
Dhineshkumar <103942237+Dhineshkumarprakasam@users.noreply.github.com>
2024-02-29 10:19:06 +0530

    Update greatest_among_2_numbers.cpp

--- class/greatest_among_2_numbers.cpp
+++ class/greatest_among_2_numbers.cpp
@@ -1,1 +1,44 @@
+#include <iostream>
+using namespace std;
+
+class great
+{
+    private:
+        int a,b,result;
+    public:
+        void read();
+        void calculate();
+        void display();
+};
+
+void great::read()
+{
+    cout<<"Enter value of a :";
+    cin>>a;
+    cout<<"Enter valud of b :";
+    cin>>b;
+}
+
+void great::calculate()
+{
+    if(a>b)
+        result=a;
+    else
+        result=b;
+}
+
+void great::display()
+{
+    cout<<"The greatest element is : "<<result;
+    cout<<endl;
+}
+
+int main()
+{
+    great obj;
+    obj.read();
+    obj.calculate();
+    obj.display();
+
+}
 
